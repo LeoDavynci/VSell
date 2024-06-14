f89547090f6ba2cff601ab34428f062575bb9f03
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-06-14 14:31:14 -0400

    Finished the just of the login page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,18 @@
-import { Box, Container, Flex, VStack } from "@chakra-ui/react"
+import { Box, Container, Flex, VStack, Text } from "@chakra-ui/react"
 import AuthForm from "../../components/AuthForm/AuthForm"
 
 const AuthPage = () => {
   return (
       <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
           <Container maxW={"container.md"} padding={0}>
-              <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+              <Flex justifyContent={"center"} alignItems={"center"} gap={20}>
                   <Box display={{ base: "none", md: "block" }}>
-                      
+                      <Box>
+                          <Text fontSize='5xl' color='White' as='b'>SELLING.</Text>
+                      </Box>
+                      <Box>
+                            <Text fontSize='5xl'color='White' as='b'>BETTER.</Text>
+                      </Box>
                   </Box>
 
                   <VStack spacing={4} align={"stretch"}>
