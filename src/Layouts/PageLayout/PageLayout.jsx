4ae81a017c01cfb0704db4b63e364708ff7a36b3
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-08-01 11:48:42 -0400

    + Added speed insights

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -3,6 +3,7 @@ import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const PageLayout = ({ children }) => {
    const { pathname } = useLocation();
@@ -15,6 +16,7 @@ const PageLayout = ({ children }) => {
          <Box flex={1} w="100%" bg="white">
             {children}
             <Analytics />
+            <SpeedInsights />
          </Box>
       </Flex>
    );
