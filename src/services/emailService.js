a6accb7c78caee836cce76e019594268cda4c283
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-09-27 17:13:09 -0500

    + Bug fix

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,6 +1,4 @@
-// emailService.js
-require("dotenv").config();
-const sgMail = require("@sendgrid/mail");
+import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendNotificationEmail = async (
