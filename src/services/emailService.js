b9a01b526431b3a200b6b49513a8510211713af3
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-09-27 17:17:12 -0500

    + Bug fix

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,26 +2,24 @@ import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async function handler(req, res) {
-   if (req.method === "POST") {
-      const { buyerName, sellerEmail, messageContent } = req.body;
+export const sendNotificationEmail = async (
+   buyerName,
+   sellerEmail,
+   messageContent
+) => {
+   const msg = {
+      to: sellerEmail,
+      from: "vsell.web@gmail.com",
+      subject: "New Buy or Offer Request",
+      text: `Hi ${buyerName},\n\n${messageContent}`,
+      html: `<strong>Hi ${buyerName},</strong><br/><p>${messageContent}</p>`,
+   };
 
-      const msg = {
-         to: sellerEmail,
-         from: "vsell.web@gmail.com",
-         subject: "New Buy or Offer Request",
-         text: `Hi ${buyerName},\n\n${messageContent}`,
-         html: `<strong>Hi ${buyerName},</strong><br/><p>${messageContent}</p>`,
-      };
-
-      try {
-         await sgMail.send(msg);
-         res.status(200).json({ message: "Email sent successfully." });
-      } catch (error) {
-         console.error("Error sending email:", error);
-         res.status(500).json({ error: "Failed to send email." });
-      }
-   } else {
-      res.status(405).json({ error: "Method not allowed" });
+   try {
+      await sgMail.send(msg);
+      console.log("Notification email sent successfully.");
+   } catch (error) {
+      console.error("Error sending email:", error);
+      throw error;
    }
-}
+};
